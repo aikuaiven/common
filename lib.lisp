4b28499cdb32888e8d513ad060ae3c64aaafaccf
aikuaiven <aikuaiven@gmail.com>
2016-10-26 11:23:53 +0300

    add

--- lib.lisp
+++ lib.lisp
@@ -1,9 +1,10 @@
 					;-*-mode: lisp; coding: cp1251;-*-
-(cffi:load-foreign-library "user32")
+(cffi:load-foreign-library "user32.dll")
 (cffi:foreign-funcall "MessageBoxW" :pointer (cffi:null-pointer) (:string :encoding :utf-16le) "Пока нету!" (:string :encoding :utf-16le) "Сообщение" :int #x40 :boolean)
-(defcfun ("MessageBoxA" message-box-a :convention :stdcall :library user32)
+(cffi:defcfun (message-box-w "MessageBoxW" :convention :stdcall :library "user32.dll")
     :boolean
     (hinstance :pointer)
     (lptext (:string :encoding :utf-16le))
     (lpcaption (:string :encoding :utf-16le))
     (utype :int))
+(message-box-w (cffi:null-pointer) "Пока нету!" "Сообщение" #x40)
